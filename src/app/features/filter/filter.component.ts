094e9f0987f230e20064eb45b0b5936a1077c27b
Jared Youtsey <jkyoutsey@bitbucket.org>
2019-01-03 16:57:23 -0600

    form controls

--- src/app/features/filter/filter.component.ts
+++ src/app/features/filter/filter.component.ts
@@ -11,7 +11,12 @@ import { takeWhile } from 'rxjs/operators';
 export class FilterComponent implements OnInit, OnDestroy {
   form: FormGroup;
 
-  deltaVs = [4, 5, 6, 7, 8, 9, 10, 11, 12];
+  readonly deltaVOptions = [4, 5, 6, 7, 8, 9, 10, 11, 12];
+  readonly tripDayOptions = [60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450];
+  readonly stayDaysOptions = [8, 16, 24, 32];
+  readonly launchWindowOptions = ['2015-2020', '2020-2025', '2025-2030', '2030-2035', '2035-2040', '2015-2040'];
+  readonly magnitudeOptions = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  readonly orbitConditionCodeOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   private destroyed = false;
 
   constructor(private formBuilder: FormBuilder) {
