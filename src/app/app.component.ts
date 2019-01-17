7fd90ea83aef49ca58d88ce0cbb8bed594c5b47c
Jared Youtsey <jkyoutsey@bitbucket.org>
2019-01-17 13:18:28 -0600

    Revert "remove unused var"
    This reverts commit f46ba8fe3e483cc6ca793cced304ad3de685eaa3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NearEarthObject } from './nasa-nhats/near-earth-object';
 
 @Component({
   selector: 'nea-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  neo: NearEarthObject;
+}
